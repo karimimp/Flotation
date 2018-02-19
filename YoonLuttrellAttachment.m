8ca9abb2dd2b160d718b231253044dbf8a3e6773
Mohsen Karimi <mohsenk@outlook.com>
2018-02-19 16:07:21 +0100

    to sync with my workstation

--- YoonLuttrellAttachment.m
+++ YoonLuttrellAttachment.m
@@ -4,10 +4,10 @@ function [Ea] = YoonLuttrellAttachment(dp,db,ub,nu_f,teta)
 %   intermediate flow.
 
 Re_b = Reb(ub,db,nu_f);
-t_i = inductionTime(teta,dp)
+t_i = inductionTime(teta,dp);
 
-num = -dp.*ub.*t_i.*(45 + 8.*Re_b.^0.72)
-dnum = 15.*db.*(dp + db)
+num = -dp.*ub.*t_i.*(45 + 8.*Re_b.^0.72);
+dnum = 15.*db.*(dp + db);
 Ea = (sin(2.*atan(exp(num./dnum)))).^2;
 
 end % end of YoonLuttrellAttachment
