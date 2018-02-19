0403559592d82397b563cda44560f28e58d6d307
Mohsen <mohsenk@outlook.com>
2018-02-19 16:43:21 +0100

    bug FIX in Schulze model

--- SchulzeStability.m
+++ SchulzeStability.m
@@ -3,7 +3,7 @@ function [Es] = SchulzeStability(dp,db,teta,sigma,g,rho_f,rho_g,rho_p,epsilon)
 %   Return the stability efficiency of bubble-particle aggregate based on
 %   Schulze formulation.
 
-omega = (180 - teta/2)*(pi/180);
+omega = (pi - teta/2);
 f_db = 4.*sigma./db - db.*rho_f.*g;
 a = 29.6.*((epsilon.^(2/3))./((dp/2 + db/2).^(1/3)));
 num = dp.^2.*((rho_f-rho_g).*g + rho_p.*a) + 1.5.*dp.*((sin(omega)).^2).*f_db;
