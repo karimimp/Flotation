4cd2eec724feb57c527baad88ac354eaa62effdb
Mohsen Karimi <mohsenk@outlook.com>
2018-02-20 20:58:21 +0100

    bug fix in YoonLuttrell Ea, dp should be in m

--- inductionTime.m
+++ inductionTime.m
@@ -1,8 +1,7 @@
 function [t] = inductionTime(teta,dp)
 % SUMMARY
 %   Returns the induction time
-teta_d = (teta/pi)*180; % convert teta to degree
 beta = 0.6;
-A = 75/teta_d;
-t = A.*(dp.^beta);
+A = 75/rad2deg(teta);  % teta degree
+t = A.*(0.01*dp.^beta); % dp in m
 end % end of inductionTime
